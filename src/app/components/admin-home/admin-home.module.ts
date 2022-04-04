a1f180f33d08d15042808552765c40e06295730d
Noelia Vasquez Crespo <noelia.vasquez@ucb.edu.bo>
2022-04-03 22:13:59 -0400

    Página principal de administrador

---
+++ src/app/components/admin-home/admin-home.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminHomeRoutingModule } from './admin-home-routing.module';
+import { AdminHomeComponent } from './admin-home.component';
+import { SharedModule } from '../../shared/shared.module';
+import { NgParticlesModule } from 'ng-particles';
+
+@NgModule({
+  declarations: [AdminHomeComponent],
+  imports: [CommonModule, AdminHomeRoutingModule, SharedModule, NgParticlesModule]
+})
+export class AdminHomeModule {}
