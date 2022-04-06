e39dcb42a677a7d2d37816d2c9cabd04e5197f04
Raul Alejandro Choque Valda <raul.choque@ucb.edu.bo>
2022-04-05 23:35:49 -0400

    Modificando el modelo de administradores

--- src/app/components/addproduct/addproduct.module.ts
+++ src/app/components/addproduct/addproduct.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AddproductRoutingModule } from './addproduct-routing.module';
 import { AddproductComponent } from './addproduct.component';
 import { SharedModule } from '../../shared/shared.module';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [AddproductComponent],
-  imports: [CommonModule, AddproductRoutingModule, SharedModule]
+  imports: [CommonModule, AddproductRoutingModule, SharedModule,FormsModule,
+    ReactiveFormsModule]
 })
 export class AddproductModule {}
--- src/app/components/auth-session/auth-admin/signup/signup.component.ts
+++ src/app/components/auth-session/auth-admin/signup/signup.component.ts
@@ -17,9 +17,8 @@ export class SignupComponent implements OnInit {
     apellido: new FormControl('', Validators.required),
     edad: new FormControl('', Validators.required),
     correoElectronico: new FormControl('', Validators.required),
-    username: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required),
-    tipoAdministradorId: new FormControl('', Validators.required)
+    userName: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
   });
 
   ngOnInit(): void {
@@ -28,6 +27,7 @@ export class SignupComponent implements OnInit {
   addNewAdmin(data: admin){
     var api = 'http://localhost:8080/administrators';
     data.status=1;
+    data.tipoAdministradorId=1;
     console.log('New Admmin: ', data);
     axios.post(api,data).then(function (result){
       console.log(result);
