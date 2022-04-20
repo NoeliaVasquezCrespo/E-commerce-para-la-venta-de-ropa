f6c69f8aaaacdedb7c2810f84e04507f4190e94f
joelalex10 <joel.alejandro.quispe.cussi@gmail.com>
2022-04-19 22:36:13 -0400

    terminando eliminacion de proveedores

--- src/app/service/productlist.service.ts
+++ src/app/service/productlist.service.ts
@@ -5,28 +5,35 @@ import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { FotosProducto } from '../models/FotosProducto';
 import { ProductDetails } from '../models/ProductDetails';
+import {product} from '../models/Product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductlistService {
-  private baseUrl:string = environment.baseUrl; 
+  private baseUrl:string = environment.baseUrl;
   constructor(private http:HttpClient){ }
 
   getListProducts():Observable<ProductDetails[]> {
     const url = `${this.baseUrl}products/details`;
     return this.http.get<ProductDetails[]>(url).pipe(
       map(
-        response => response, error => error)); ;  
+        response => response, error => error)); ;
   }
   getFirstImageByProductId(idProducto:number):Observable<FotosProducto>{
     const url = `${this.baseUrl}products/image/${idProducto}`;
     return this.http.get<FotosProducto>(url).pipe(
       map(
-        response => response, error => error)); 
+        response => response, error => error));
+  }
+  getListProductsByProviderId(idProvider:number):Observable<product[]>{
+    const url = `${this.baseUrl}products/idAdmin=${idProvider}`;
+    return this.http.get<product[]>(url).pipe(
+      map(
+        response => response, error => error));
   }
   // getRouteImage(path:string, name:string){
   //   const url = `${this.baseUrl}products/image/${idProducto}`;
-    
+
   // }
 }
